85f21e4abf3e1007d9d5f72051ca43986fa9fde6
procansoft <selcukcankurt@hotmail.com>
2021-11-07 16:53:58 +0500

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ import gunicorn
 from os import environ
 from FlaskWebApp import app
 
+"""
+The flask application package.
+"""
+import gunicorn
+from flask import Flask
+app = Flask(__name__)
+
+import FlaskWebApp.views
+
+
 if __name__ == '__main__':
     
     app.run()
